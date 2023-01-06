2bf1203f462b69b7833486c318e3ee3ab98f9929
Ivan Drobyshev <drobyshev_ivan00@mail.ru>
2023-01-06 12:55:25 +0500

    added get_size in ArmaniParserCard

--- service/parser/Armani/ArmaniParserCard.py
+++ service/parser/Armani/ArmaniParserCard.py
@@ -1,3 +1,8 @@
+import asyncio
+import time
+
+from selenium.webdriver.common.by import By
+
 from service.parser.Armani.ArmaniParser import ArmaniParser
 from time import sleep
 from bs4 import BeautifulSoup
@@ -15,31 +20,58 @@ class ArmaniParserCard(ArmaniParser):
             card = await self.open_url(link)
             try:
                 id_product = card.find(class_="modelFabricColor").find("span", class_="value").text
-                desc = card.find("div", class_="item-info__accordion-content").text.replace("Product Code", "").replace(
-                    id_product, "").strip()
             except:
                 id_product = None
-                desc = None
-            # size =  # размеры
-            colors = await self.get_color(link)  # цвета
-            composition = await self.get_detail(card)  # состав
+            try:
+                description = card.find("div", class_="item-info__accordion-content").text.replace("Product Code", "").replace(
+                    id_product, "").strip()
+            except:
+                description = None
+            colors = await self.__get_color(link)  # цвета
+            size = await self.__get_size(link)
+            composition = await self.__get_composition(card)  # состав
             self.update_db.update_items_card(link,
                                              id_product=id_product,
-                                             description=desc,
+                                             description=description,
                                              composition=composition,
+                                             size=size,
                                              colors=colors)
             print(f"[INFO]Продукт {link} в публикации")
 
     @staticmethod
-    async def get_detail(card):
+    async def __get_composition(card):
         composition = []
-        values = card.find("div", attrs={"data-ytos-tab": "Detail-Features"}).find_all("li")
-        for li in values:
-            composition.append(li.text.strip())
-        return ". ".join(composition)
+        try:
+            values = card.find("div", attrs={"data-ytos-tab": "Detail-Features"}).find_all("li")
+            for li in values:
+                composition.append(li.text.strip())
+            return ". ".join(composition)
+        except:
+            return None
 
     @staticmethod
-    async def get_color(url_card):
+    async def __get_size(url):
+        size = []
+        driver = await Proxy().get_chromedriver(
+            user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+        print(f'[INFO]Открываем страницу {url}')
+        driver.get(url)
+        time.sleep(10)
+        try:
+            element = driver.find_element(By.XPATH, '//*[@id="itemInfo"]/div[4]/div[1]/div[1]/div[3]/div/div[1]')
+            element.click()
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'lxml')
+            div_size = soup.find_all("div", "option")
+            for s in div_size:
+                size.append(s.text)
+            res = ".".join(size)
+            return res.replace("Size.", "")
+        except:
+            return None
+
+    @staticmethod
+    async def __get_color(url_card):
         colors = []
         driver = await Proxy().get_chromedriver(
             user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
